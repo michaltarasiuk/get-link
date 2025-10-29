868d8b4940da4590afb49697e059bd08ede23053
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-30 00:26:55 +0100

    update sidebar styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,8 +17,10 @@ export function Sidebar() {
   return (
     <nav
       className={cn(
-        "bg-sidebar flex gap-1 px-2 py-1",
-        "md:border-sidebar-border md:flex-col md:border-e md:p-1 md:py-2",
+        "bg-sidebar relative flex gap-1 px-2 py-1.5",
+        "before:bg-sidebar before:absolute before:start-0 before:top-0 before:h-4 before:w-full before:-translate-y-full",
+        "after:bg-background after:absolute after:start-0 after:top-0 after:h-4 after:w-full after:-translate-y-full after:rounded-b-4xl",
+        "md:border-sidebar-border md:flex-col md:border-e md:p-1 md:py-2 md:before:hidden md:after:hidden",
       )}>
       <Button
         variant={getButtonVariant(Routes.save)}
