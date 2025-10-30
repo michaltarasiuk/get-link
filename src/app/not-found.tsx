a497c16848b528264e8ab6ff9e34e79994082d76
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-30 14:57:33 +0100

    add size-full class to NotFound component for improved layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ import {cn} from "@/lib/cn";
 
 export default function NotFound() {
   return (
-    <div className={cn("flex items-center justify-center")}>
+    <div className={cn("flex size-full items-center justify-center")}>
       <Empty>
         <EmptyHeader>
           <EmptyTitle>404 - Not Found</EmptyTitle>
