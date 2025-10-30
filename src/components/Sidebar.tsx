fed058af7367d50894f800106f4cfcde76d79343
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-30 09:25:30 +0100

    refactor Sidebar component for improved structure and readability

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,36 +10,47 @@ import {Routes} from "@/lib/routes";
 import {Button} from "./ui/button";
 
 export function Sidebar() {
-  const pathname = usePathname();
-  function getButtonVariant(href: string) {
-    return href === pathname ? "secondary" : "ghost";
-  }
   return (
     <nav
       className={cn(
-        "bg-sidebar relative flex gap-1 px-2 py-1.5",
-        "before:bg-sidebar before:absolute before:start-0 before:top-0 before:h-4 before:w-full before:-translate-y-full",
-        "after:bg-background after:absolute after:start-0 after:top-0 after:h-4 after:w-full after:-translate-y-full after:rounded-b-4xl",
-        "md:border-sidebar-border md:flex-col md:border-e md:p-1 md:py-2 md:before:hidden md:after:hidden",
+        "bg-sidebar relative",
+        "before:bg-sidebar before:block before:h-4",
+        "after:bg-background after:absolute after:top-0 after:h-4 after:w-full after:rounded-b-4xl",
+        "md:border-sidebar-border md:border-e md:before:hidden md:after:hidden",
       )}>
-      <Button
-        variant={getButtonVariant(Routes.save)}
-        className={cn("flex h-16 w-20 flex-col")}
-        asChild>
-        <Link href={Routes.save}>
-          <DownloadIcon />
-          save
-        </Link>
-      </Button>
-      <Button
-        variant={getButtonVariant(Routes.settings)}
-        className={cn("flex h-16 w-20 flex-col")}
-        asChild>
-        <Link href={Routes.settings}>
-          <SettingsIcon />
-          settings
-        </Link>
-      </Button>
+      <ul
+        className={cn("flex gap-1 px-2 py-1.5", "md:flex-col md:p-1 md:py-2")}>
+        <li>
+          <SidebarLink href={Routes.save}>
+            <DownloadIcon />
+            save
+          </SidebarLink>
+        </li>
+        <li>
+          <SidebarLink href={Routes.settings}>
+            <SettingsIcon />
+            settings
+          </SidebarLink>
+        </li>
+      </ul>
     </nav>
+  );
+}
+
+function SidebarLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+    <Button
+      variant={href === pathname ? "secondary" : "ghost"}
+      className={cn("flex h-16 w-20 flex-col")}
+      asChild>
+      <Link href={href}>{children}</Link>
+    </Button>
   );
 }
