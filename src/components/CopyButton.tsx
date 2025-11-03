552aaa4f3a996db1b3f37f3e072c181b7a6ef3de
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-03 23:08:12 +0100

    enhance CopyButton component with copy functionality and animations

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,11 +1,41 @@
-import {LinkIcon} from "lucide-react";
+"use client";
+
+import {CheckIcon, LinkIcon} from "lucide-react";
+import {useEffect, useRef, useState} from "react";
+
+import {cn} from "@/lib/cn";
 
 import {Button} from "./ui/button";
 
 export function CopyButton() {
+  const [copied, setCopied] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout>(undefined);
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+  function handleClick() {
+    setCopied(true);
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setCopied(false);
+    }, 2_000);
+  }
   return (
-    <Button variant="ghost" size="icon-sm">
-      <LinkIcon />
+    <Button variant="ghost" size="icon-sm" onClick={handleClick}>
+      <LinkIcon
+        className={cn(
+          "transition-transform ease-in-out",
+          copied ? "scale-0" : "scale-100",
+        )}
+      />
+      <CheckIcon
+        className={cn(
+          "absolute transition-transform ease-in-out",
+          copied ? "scale-100" : "scale-0",
+        )}
+      />
     </Button>
   );
 }
