fbc4db9c26ae872d94c84a2e948f5df67f827c26
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-04 13:59:52 +0100

    refactor: extract Header component from PageLayout for improved readability and structure

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -10,17 +10,28 @@ interface PageLayoutProps {
 export function PageLayout({title, children, header}: PageLayoutProps) {
   return (
     <div className={cn("pt-12", "sm:pt-0")}>
-      <header
-        className={cn(
-          "bg-background fixed top-0 flex h-12 w-full items-center justify-center",
-          "sm:hidden",
-        )}>
-        {isDefined(header) && (
-          <div className={cn("absolute start-2")}>{header}</div>
-        )}
-        <h1 className={cn("text-lg font-semibold tracking-wide")}>{title}</h1>
-      </header>
+      <Header title={title}>{header}</Header>
       {children}
     </div>
+  );
+}
+
+interface HeaderProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+function Header({title, children}: HeaderProps) {
+  return (
+    <header
+      className={cn(
+        "bg-background fixed top-0 flex h-12 w-full items-center justify-center",
+        "sm:hidden",
+      )}>
+      {isDefined(children) && (
+        <div className={cn("absolute start-2")}>{children}</div>
+      )}
+      <h1 className={cn("text-lg font-semibold tracking-wide")}>{title}</h1>
+    </header>
   );
 }
