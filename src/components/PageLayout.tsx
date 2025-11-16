59a702e15fb5aefa3a8a3be1ac96dd758ac30f60
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-16 13:10:50 +0100

    refactor: simplify PageLayout component structure and extract BackLink function

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -6,11 +6,10 @@ import {isDefined} from "@/lib/is-defined";
 
 import {Button} from "./ui/button";
 
-interface PageLayout extends React.ComponentProps<typeof Header> {
-  children: React.ReactNode;
-}
-
-export function PageLayout({children, ...props}: PageLayout) {
+export function PageLayout({
+  children,
+  ...props
+}: React.ComponentProps<typeof Header> & {children: React.ReactNode}) {
   return (
     <div
       className={cn("pt-12", "md:mb-0 md:pt-0", {
@@ -29,18 +28,22 @@ function Header({title, backTo}: {title: string; backTo?: string}) {
         "bg-background fixed top-0 z-50 flex h-12 w-full items-center justify-center",
         "md:hidden",
       )}>
-      {isDefined(backTo) && (
-        <Button
-          variant="ghost"
-          size="icon-sm"
-          className={cn("absolute start-1.5")}
-          asChild>
-          <Link href={backTo}>
-            <ArrowLeftIcon aria-hidden className={cn("size-5")} />
-          </Link>
-        </Button>
-      )}
+      {isDefined(backTo) && <BackLink href={backTo} />}
       <h1 className={cn("text-lg font-semibold tracking-wide")}>{title}</h1>
     </header>
+  );
+}
+
+function BackLink({href}: {href: string}) {
+  return (
+    <Button
+      variant="ghost"
+      size="icon-sm"
+      className={cn("absolute start-1.5")}
+      asChild>
+      <Link href={href}>
+        <ArrowLeftIcon aria-hidden className={cn("size-5")} />
+      </Link>
+    </Button>
   );
 }
