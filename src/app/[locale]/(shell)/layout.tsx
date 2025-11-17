62ef4463fbcb02ddaf5545a86332a206f8674b6a
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-17 15:54:50 +0100

    update layout styles for improved responsiveness and structure

--- src/app/[locale]/(shell)/layout.tsx
+++ src/app/[locale]/(shell)/layout.tsx
@@ -4,9 +4,9 @@ import {cn} from "@/lib/cn";
 
 export default function ShellLayout({children}: LayoutProps<"/[locale]">) {
   return (
-    <div className={cn("flex min-h-dvh flex-col", "md:flex-row-reverse")}>
-      <div className={cn("flex flex-1 flex-col")}>{children}</div>
+    <div className={cn("flex min-h-dvh flex-col-reverse", "md:flex-row")}>
       <Sidebar />
+      <div className={cn("flex flex-1 flex-col")}>{children}</div>
       <ProcessingQueue />
     </div>
   );
