7c16c519707a1661f383c645c54e2af833ffb1c0
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-22 00:56:03 +0100

    refactor: enhance ShellLayout structure and improve accessibility

--- src/app/[locale]/(shell)/layout.tsx
+++ src/app/[locale]/(shell)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import dedent from "dedent";
+
 import {ProcessingQueue} from "@/components/ProcessingQueue";
 import {Sidebar} from "@/components/Sidebar";
 import {useIsServer} from "@/hooks/use-is-server";
@@ -8,17 +10,28 @@ import {cn} from "@/lib/cn";
 export default function ShellLayout({children}: LayoutProps<"/[locale]">) {
   const isServer = useIsServer();
   return (
-    <div
-      className={cn(
-        "flex min-h-dvh flex-col-reverse",
-        "md:flex-row",
-        isServer
-          ? "opacity-0"
-          : "opacity-100 transition-opacity duration-200 ease-out",
-      )}>
-      <Sidebar />
-      <div className={cn("flex flex-1 flex-col")}>{children}</div>
-      <ProcessingQueue />
-    </div>
+    <>
+      <div
+        id="shell"
+        className={cn(
+          "flex min-h-dvh flex-col-reverse md:flex-row",
+          isServer
+            ? "opacity-0"
+            : "opacity-100 transition-opacity duration-200 ease-out",
+        )}>
+        <Sidebar />
+        <main className="flex flex-1 flex-col">{children}</main>
+        <ProcessingQueue />
+      </div>
+      <noscript>
+        <style>
+          {dedent`
+            #shell {
+              opacity: 100;
+            }
+          `}
+        </style>
+      </noscript>
+    </>
   );
 }
